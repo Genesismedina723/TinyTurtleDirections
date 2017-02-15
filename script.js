8603ac2042f8e0241df1e4daf1a0133f05e30fd4
Genesis Medina <genesismedina723@gmail.com>
2017-02-15 15:17:10 +0000

    I gave the turtle directions

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ TinyTurtle.apply(window);
 // Write directions for the tiny turtle to make
 // the shape of a house by using the following methods below:
 //
-// forward();
-// right();
-// left();
-// stamp();
+forward(40); //walk forward 4;
+right(90);
+forward(80);
+right(90);
+forward(40);
+right(90);
+forward(81);
+right(90);
+stamp();
 //
 
